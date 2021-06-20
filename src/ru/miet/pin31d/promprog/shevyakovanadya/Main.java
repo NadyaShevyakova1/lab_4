05a24105543abe7118c571b96dc0918461d9ca7c
Nadya Shevyakova <nadya-shevyakova@mail.ru>
2021-06-20 17:45:03 +0300

    Класс прямоуголник. Формула площади и перемитра

--- src/ru/miet/pin31d/promprog/shevyakovanadya/Main.java
+++ src/ru/miet/pin31d/promprog/shevyakovanadya/Main.java
@@ -1,8 +1,22 @@
 package ru.miet.pin31d.promprog.shevyakovanadya;
 
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
+import java.util.Scanner;
+class Rectangle {
+    public double side1,side2;
+    public Rectangle(){}
+    //Формула для нахождения площади
+    public double areaCalculator(double num1,double num2){
+        side1 = (num1 * num2);
+        return side1;
+    }
+    //Формула для нахождения периметра прямоугольника
+    public double perimeterCalculator(double num1,double num2){
+        side2 = (num1 + num2)*2;
+        return side2;
+    }
+    public void viewResult(){
+        System.out.println("Площадь прямоугольника: "+side1);
+        System.out.println("Периметра прямоугольника: "+side2);
     }
 }
+
