35e2837ea6e0e55f9af1975f2f57428225520c54
Nadya Shevyakova <nadya-shevyakova@mail.ru>
2021-06-20 17:48:29 +0300

    Класс квадрат. Формула площади и перемитра

--- src/ru/miet/pin31d/promprog/shevyakovanadya/Main.java
+++ src/ru/miet/pin31d/promprog/shevyakovanadya/Main.java
@@ -20,5 +20,22 @@ class Rectangle {
     }
 }
 
-
+class Square {
+    public double sideSquare1, sideSquare2;
+    public Square(){}
+    //Формула для нахождения площади
+    public double areaSquare(double x){
+        sideSquare1 = (x*x);
+        return sideSquare1;
+    }
+    //Формула для нахождения периметра прямоугольника
+    public double perimeterSquare(double x){
+        sideSquare2 = (x+x)*2;
+        return sideSquare2;
+    }
+    public void viewResultSquare(){
+        System.out.println("Площадь квадрата: "+sideSquare1);
+        System.out.println("Периметра квадрата: "+sideSquare2);
+    }
+}
 
